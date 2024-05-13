a65bc5132c3bed4e40fe44031130b0a38e348f60
Andreas Dymek <andreas.dymek@rwth-aachen.de>
2024-05-13 09:59:09 +0200

    Basic Layout Added to Widget

--- widgets/webwriter-branching-scenario.ts
+++ widgets/webwriter-branching-scenario.ts
@@ -1,13 +1,44 @@
 import { html, css, LitElement } from "lit";
 import { LitElementWw } from "@webwriter/lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
 
 @customElement("webwriter-branching-scenario")
 export class WebwriterBranchingScenario extends LitElementWw {
+  static styles = css`
+    .container {
+      border-style: solid;
+      border-color: #000000;
+      border-width: 1px;
+      border-radius: 8px;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .controls {
+      /* Styles for special div */
+      background-color: #f0f0f0;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+      gap: 10px; /* Adjust the value to your desired spacing */
+      padding: 10px;
+    }
+  `;
+
   render() {
-    return html` <div>
-      <h1>Test</h1>
-      <p>Hello, world!</p>
-    </div>`;
+    console.log("render", this);
+    return html` <head> </head>
+      <body>
+        <div class="container">
+          <div>Graph</div>
+          <div class="controls">
+            <button>Zoom In</button>
+            <button>Zoom Out</button>
+            <button>Add Sheet</button>
+            <button>Add Branch</button>
+          </div>
+          <div>Sheet</div>
+        </div>
+      </body>`;
   }
 }
