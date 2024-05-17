173c980eed6ce00a61535e7a4f47b48ef1a40819
Andreas Dymek <andreas.dymek@rwth-aachen.de>
2024-05-17 09:54:37 +0200

    Use Lit Event Handling

--- widgets/webwriter-branching-scenario.ts
+++ widgets/webwriter-branching-scenario.ts
@@ -18,6 +18,7 @@ import JournalPlus from "bootstrap-icons/icons/journal-plus.svg";
 import ArrowCounterClockWise from "bootstrap-icons/icons/arrow-counterclockwise.svg";
 import ArrowClockWise from "bootstrap-icons/icons/arrow-clockwise.svg";
 import Trash3 from "bootstrap-icons/icons/trash3.svg";
+import Floppy from "bootstrap-icons/icons/floppy.svg";
 
 //Drawflow Imports
 import Drawflow from "drawflow";
@@ -35,143 +36,70 @@ export class WebWriterBranchingScenario extends LitElementWw {
   @query("#drawflow")
   drawflow;
 
-  static scopedElements = {
-    "sl-button": SlButton,
+  @property()
+  editor?: Drawflow;
 
-    "sl-divider": SlDivider,
-    "sl-dialog": SlDialog,
-    "sl-icon-button": SlIconButton,
-  };
+  static get scopedElements() {
+    return {
+      "sl-button": SlButton,
+      "sl-textarea": SlTextarea,
+      "sl-divider": SlDivider,
+      "sl-dialog": SlDialog,
+      "sl-icon-button": SlIconButton,
+    };
+  }
+
+  // static scopedElements = {
+  //   "sl-button": SlButton,
+  //   "sl-textarea": SlTextarea,
+  //   "sl-divider": SlDivider,
+  //   "sl-dialog": SlDialog,
+  //   "sl-icon-button": SlIconButton,
+  // };
 
   static styles = [style, styles];
 
   protected firstUpdated(_changedProperties: any): void {
     const container = this.shadowRoot?.getElementById("drawflow");
-    const editor = new Drawflow(container);
-    editor.reroute = true;
-    editor.reroute_fix_curvature = true;
-    editor.start();
-    // Event listeners for buttons
-    this.shadowRoot
-      .getElementById("zoomInBtn")
-      .addEventListener("click", () => {
-        editor.zoom_in();
-      });
-    this.shadowRoot
-      .getElementById("zoomOutBtn")
-      .addEventListener("click", () => {
-        editor.zoom_out();
-      });
-    this.shadowRoot
-      .getElementById("addSheetBtn")
-      .addEventListener("click", () => {
-        i_count_new_sheets++;
-        addSheetNode(editor, `Untitled Sheet ${i_count_new_sheets}`);
-      });
-    this.shadowRoot
-      .getElementById("addBranchBtn")
-      .addEventListener("click", () => {
-        addBranchNode(editor, `Branch`);
-      });
-    this.shadowRoot.getElementById("clearBtn").addEventListener("click", () => {
-      const dialog = this.shadowRoot.getElementById("dialog") as SlDialog;
-      dialog.show();
-    });
-    this.shadowRoot
-      .getElementById("closeDialogBtn")
-      .addEventListener("click", () => {
-        const dialog = this.shadowRoot.getElementById("dialog") as SlDialog;
-        dialog.hide();
-      });
-    this.shadowRoot
-      .getElementById("clearConfirmBtn")
-      .addEventListener("click", () => {
-        const dialog = this.shadowRoot.getElementById("dialog") as SlDialog;
-        dialog.hide();
-        editor.clear();
-      });
-    //TOOD: change to @
-    this.shadowRoot
-      .getElementById("saveChangesBtn")
-      .addEventListener("click", () => {
-        const textAreaHTML = this.shadowRoot?.getElementById(
-          "textAreaHTML"
-        ) as SlTextarea;
-        const selectedNode = editor.getNodeFromId(selected_node_id);
-        const newHTML = textAreaHTML.value;
-        editor.updateNodeDataFromId(selected_node_id, {
-          name: selectedNode.data.name,
-          html: newHTML,
-        });
-      });
-    editor.on("nodeDataChanged", (id) => {
+    this.editor = new Drawflow(container);
+    this.editor.reroute = true;
+    this.editor.reroute_fix_curvature = true;
+
+    this.editor.start();
+
+    //register editor event
+    this.editor.on("nodeDataChanged", (id) => {
       console.log("nodeDataChanged");
-      console.log(editor.getNodeFromId(id));
+      console.log(this.editor.getNodeFromId(id));
     });
+
     // Event listener for node click
-    editor.on("nodeSelected", (id) => {
+    this.editor.on("nodeSelected", (id) => {
       console.log("node selected");
-      const node = editor.getNodeFromId(id);
+      const node = this.editor.getNodeFromId(id);
       const textAreaHTML = this.shadowRoot?.getElementById(
         "textAreaHTML"
       ) as SlTextarea;
       textAreaHTML.value = node.data.html;
       selected_node_id = id;
     });
+
     // Event listener for node click
-    editor.on(
+    this.editor.on(
       "connectionSelected",
       (output_id, input_id, output_class, input_class) => {
         console.log("connection selected");
       }
     );
+
     // Event listener for node click
-    editor.on("nodeUnselected", (boolean) => {
+    this.editor.on("nodeUnselected", (boolean) => {
       const textAreaHTML = this.shadowRoot?.getElementById(
         "textAreaHTML"
       ) as SlTextarea;
       textAreaHTML.value = "";
       selected_node_id = null;
     });
-    function addSheetNode(editor, name) {
-      console.log(name);
-      const data = {
-        name: name,
-        html: `<div><p>Testing HTML Editing</p></div>`,
-      };
-
-      // Create a new div element and set its inner HTML
-      const tempDiv = document.createElement("div");
-      tempDiv.innerHTML = `
-      <div>
-        <div class="title-box"><p>Worksheet</p></div>
-        <div class="content">
-          <button>test</button>
-          <sl-textarea rows="1" resize="none" value="${name}" placeholder="Enter name" size="small" label="Name" df-name></sl-textarea>
-        </div>
-      </div>`;
-
-      //editor.addNode(name, inputs, outputs, posx, posy, class, data, html);
-      editor.addNode(name, 0, 1, 0, 0, "sheet", data, tempDiv.innerHTML);
-    }
-
-    function addBranchNode(editor, type) {
-      const data = {
-        name: type,
-      };
-
-      //editor.addNode(name, inputs, outputs, posx, posy, class, data, html);
-      editor.addNode(
-        type,
-        1,
-        2,
-        0,
-        0,
-        "branch",
-        data,
-        `\n            <div>\n              <div class="title-box">${type} </div>\n            </div>\n            `
-      );
-    }
   }
 
   render() {
@@ -180,18 +108,36 @@ export class WebWriterBranchingScenario extends LitElementWw {
           <sl-icon-button
             id="addSheetBtn"
             src=${FileEarmarkPlus}
+            @click=${() => this._addSheetNode("Untitled Sheet")}
           ></sl-icon-button>
-          <sl-icon-button id="addBranchBtn" src=${JournalPlus}></sl-icon-button>
+          <sl-icon-button
+            id="addBranchBtn"
+            src=${JournalPlus}
+            @click=${() => this._addBranchNode("Branch")}
+          ></sl-icon-button>
           <sl-divider vertical style="height: 30px;"></sl-divider>
           <sl-icon-button src=${ArrowCounterClockWise}> </sl-icon-button>
           <sl-icon-button src=${ArrowClockWise}></sl-icon-button>
           <sl-divider vertical style="height: 30px;"></sl-divider>
-          <sl-icon-button id="clearBtn" src=${Trash3}></sl-icon-button>
+          <sl-icon-button
+            id="clearBtn"
+            src=${Trash3}
+            @click=${() =>
+              (this.shadowRoot.getElementById("dialog") as SlDialog).show()}
+          ></sl-icon-button>
         </div>
         <div id="drawflow">
           <div class="bar-zoom">
-            <sl-icon-button id="zoomInBtn" src=${ZoomIn}></sl-icon-button>
-            <sl-icon-button id="zoomOutBtn" src=${ZoomOut}></sl-icon-button>
+            <sl-icon-button
+              id="zoomInBtn"
+              src=${ZoomIn}
+              @click=${() => this.editor.zoom_in()}
+            ></sl-icon-button>
+            <sl-icon-button
+              id="zoomOutBtn"
+              src=${ZoomOut}
+              @click=${() => this.editor.zoom_out()}
+            ></sl-icon-button>
           </div>
         </div>
 
@@ -203,19 +149,114 @@ export class WebWriterBranchingScenario extends LitElementWw {
             placeholder="No node selected"
             size="large"
           ></sl-textarea>
-          <sl-icon-button name="floppy" id="saveChangesBtn"></sl-icon-button>
+          <sl-icon-button
+            src=${Floppy}
+            id="saveChangesBtn"
+            @click=${this._saveChangesToNodeData}
+          ></sl-icon-button>
         </div>
       </div>
 
       <sl-dialog label="Clear graph" class="dialog" id="dialog">
         Do you want to clear the graph? All your progress will be lost.
-        <sl-button id="closeDialogBtn" slot="footer" variant="primary" outline
+        <sl-button
+          id="closeDialogBtn"
+          slot="footer"
+          variant="primary"
+          outline
+          @click=${() =>
+            (this.shadowRoot.getElementById("dialog") as SlDialog).hide()}
           >Cancel</sl-button
         >
-        <sl-button id="clearConfirmBtn" slot="footer" variant="danger" outline
+        <sl-button
+          id="clearConfirmBtn"
+          slot="footer"
+          variant="danger"
+          outline
+          @click=${() => this._clearEditor}
           >Clear</sl-button
         >
       </sl-dialog>`;
+  }
+
+  //TODO: make this.editor a state variable, change the html render to include @click event catcher, write the funciton outside firstRender, see if that fixes sl-bug in node
+  private _addSheetNode(name) {
+    const data = {
+      name: name,
+      html: `<div><p>Testing HTML Editing</p></div>`,
+    };
+
+    // Create a new div element and set its inner HTML
+
+    //this.editor.addNode(name, inputs, outputs, posx, posy, class, data, html);
+    this.editor.addNode(
+      name,
+      0,
+      1,
+      0,
+      0,
+      "sheet",
+      data,
+      ` <div>
+        <div class="title-box">
+          <p>Worksheet</p>
+        </div>
+        <div class="content">
+          <button>test</button>
+          <sl-textarea
+            rows="1"
+            resize="none"
+            value="${name}"
+            placeholder="Enter name"
+            size="small"
+            label="Name"
+            df-name
+          ></sl-textarea>
+        </div>
+      </div>`,
+      false
+    );
+  }
+
+  private _addBranchNode(type) {
+    const data = {
+      name: type,
+    };
+
+    //this.editor.addNode(name, inputs, outputs, posx, posy, class, data, html);
+    this.editor.addNode(
+      type,
+      1,
+      2,
+      0,
+      0,
+      "branch",
+      data,
+      `
+        <div>
+          <div class="title-box">${type}</div>
+        </div>
+      `,
+      false
+    );
+  }
+
+  private _clearEditor() {
+    const dialog = this.shadowRoot.getElementById("dialog") as SlDialog;
+    dialog.hide();
+    this.editor.clear();
+  }
+
+  private _saveChangesToNodeData() {
+    const textAreaHTML = this.shadowRoot?.getElementById(
+      "textAreaHTML"
+    ) as SlTextarea;
+    const selectedNode = this.editor.getNodeFromId(selected_node_id);
+    const newHTML = textAreaHTML.value;
+    this.editor.updateNodeDataFromId(selected_node_id, {
+      name: selectedNode.data.name,
+      html: newHTML,
+    });
   }
 }
 
