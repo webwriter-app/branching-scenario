45c14e2d2e349b7f1dd4825a22848d0c42dfe2ea
Andreas Dymek <andreas.dymek@rwth-aachen.de>
2024-07-25 11:45:48 +0200

    Added visual options for connection button

--- widgets/webwriter-connection-button.ts
+++ widgets/webwriter-connection-button.ts
@@ -1,28 +1,31 @@
-import { html, css, LitElement, unsafeCSS } from "lit";
-import {
-  customElement,
-  property,
-  query,
-  state,
-  queryAssignedElements,
-} from "lit/decorators.js";
+import { html, css, LitElement } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
 import { LitElementWw } from "@webwriter/lit";
 
 import "@shoelace-style/shoelace/dist/themes/light.css";
-import { SlOption, SlSelect, SlButton } from "@shoelace-style/shoelace";
+import {
+  SlOption,
+  SlSelect,
+  SlButton,
+  SlRange,
+} from "@shoelace-style/shoelace";
 
 @customElement("webwriter-connection-button")
 export class WebWriterConnectionButton extends LitElementWw {
-  @property({ type: String }) name: string;
+  @property({ type: String }) name: string = "Button";
   @property({ type: Number }) dataTargetId: number;
-  @property({ type: String, attribute: true })
-  identifier: string;
+  @property({ type: String, attribute: true }) identifier: string;
+  @state() variant: string = "default";
+  @state() size: string = "medium";
+  @state() pill: boolean = false;
+  @state() width: number = 100;
 
   static get scopedElements() {
     return {
       "sl-button": SlButton,
       "sl-select": SlSelect,
       "sl-option": SlOption,
+      "sl-range": SlRange,
     };
   }
 
@@ -50,12 +53,31 @@ export class WebWriterConnectionButton extends LitElementWw {
     `;
   }
 
-  //TODO: Copy and Paste and stuff does not make sense if the editor does not react
-  //TODO: Focus is messed up, when clicking on the input, the focus shifts away from the button
+  handleVariantChange(e) {
+    this.variant = e.target.value;
+  }
+
+  handleSizeChange(e) {
+    this.size = e.target.value;
+  }
+
+  handlePillChange(e) {
+    this.pill = e.target.checked;
+  }
+
+  handleWidthChange(e) {
+    this.width = e.target.value;
+  }
+
   render() {
     return html`
       <div style="width: 100%">
-        <sl-button size="medium" style="padding: 0px; width: 100%;">
+        <sl-button
+          size=${this.size}
+          style="padding: 0px; width: ${this.width}%;"
+          variant=${this.variant}
+          ?pill=${this.pill}
+        >
           <p>${this.name}</p>
         </sl-button>
         <div part="options" class="controls">
@@ -66,14 +88,33 @@ export class WebWriterConnectionButton extends LitElementWw {
             @change=${(e) => (this.name = e.target.value)}
           />
           <p>Style</p>
-          <sl-select>
-            <sl-option value="option-1">Option 1</sl-option>
-            <sl-option value="option-2">Option 2</sl-option>
-            <sl-option value="option-3">Option 3</sl-option>
-            <sl-option value="option-4">Option 4</sl-option>
-            <sl-option value="option-5">Option 5</sl-option>
-            <sl-option value="option-6">Option 6</sl-option>
+          <sl-select @sl-change=${this.handleVariantChange}>
+            <sl-option value="default">Default</sl-option>
+            <sl-option value="primary">Primary</sl-option>
+            <sl-option value="success">Success</sl-option>
+            <sl-option value="neutral">Neutral</sl-option>
+            <sl-option value="warning">Warning</sl-option>
+            <sl-option value="danger">Danger</sl-option>
           </sl-select>
+          <p>Size</p>
+          <sl-select @sl-change=${this.handleSizeChange}>
+            <sl-option value="small">Small</sl-option>
+            <sl-option value="medium">Medium</sl-option>
+            <sl-option value="large">Large</sl-option>
+          </sl-select>
+          <p>Pill</p>
+          <input
+            type="checkbox"
+            .checked=${this.pill}
+            @change=${this.handlePillChange}
+          />
+          <p>Width</p>
+          <sl-range
+            min="10"
+            max="100"
+            value=${this.width}
+            @input=${this.handleWidthChange}
+          ></sl-range>
         </div>
       </div>
     `;
