ebc03fa0394120d11a4f815759c647b6606633a5
Andreas Dymek <andreas.dymek@rwth-aachen.de>
2024-08-14 12:00:45 +0200

    Output Connection Control hides filtered results via css display none, thus keeping its state

--- widgets/ui-components/output-connection-control.ts
+++ widgets/ui-components/output-connection-control.ts
@@ -1,8 +1,5 @@
-import { html, css, LitElement, unsafeCSS, PropertyValues } from "lit";
-import { LitElementWw } from "@webwriter/lit";
+import { html, css, LitElement, PropertyValues } from "lit";
 import { customElement, property, state, query } from "lit/decorators.js";
-
-//Shoelace Imports
 import "@shoelace-style/shoelace/dist/themes/light.css";
 import {
   SlSelect,
@@ -11,10 +8,7 @@ import {
   SlIcon,
   SlInput,
 } from "@shoelace-style/shoelace";
-
-//Drawflow Imports
 import Drawflow, { DrawflowNode } from "drawflow";
-
 import file from "@tabler/icons/outline/file.svg";
 import squares from "@tabler/icons/outline/squares.svg";
 import arrowsSplit2 from "@tabler/icons/outline/arrows-split-2.svg";
@@ -34,348 +28,331 @@ const NO_NODE_SELECTED: DrawflowNode = {
 };
 
 @customElement("output-connection-control")
-export class OutputConnectionControl extends LitElementWw {
-  @property({ type: Object, attribute: true, reflect: false }) nodeEditor;
-  @property({ type: Object, attribute: true, reflect: false })
-  selectedNode;
-  @property({ type: String, attribute: true, reflect: true })
-  outputClass?;
+export class OutputConnectionControl extends LitElement {
+  @property({ type: Object }) nodeEditor;
+  @property({ type: Object }) selectedNode;
+  @property({ type: String }) outputClass?;
 
   @state() searchTerm = "";
 
-  @query("sl-input")
-  searchElement!: SlInput;
+  @query("sl-input") searchElement!: SlInput;
+  @query("sl-select") selectElement!: SlSelect;
 
-  @query("sl-select")
-  selectElement!: SlSelect;
+  static styles = css`
+    .nodeSelect {
+      width: 100%;
+    }
+    .node-option-visible {
+      display: block;
+    }
+    .node-option-hidden {
+      display: none;
+    }
+    sl-select {
+      --sl-input-border-width: 0px;
+      --sl-input-padding: 0px;
+    }
+    sl-select::part(listbox) {
+      width: 250px;
+      height: 250px;
+    }
+    sl-select::part(display-input) {
+      border: none;
+      font-weight: 500;
+      color: #0084c7;
+      font-size: 12px;
+      width: 70px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      text-align: left;
+    }
+    .icon-header {
+      display: flex;
+      align-items: center;
+      gap: 7px;
+    }
+    .test {
+      --sl-input-border-width: 1px;
+    }
+  `;
 
-  //registering custom elements used in the widget
-  static get scopedElements() {
-    return {
-      "sl-select": SlSelect,
-      "sl-option": SlOption,
-      "sl-divider": SlDivider,
-      "sl-icon": SlIcon,
-      "sl-input": SlInput,
-    };
-  }
+  /*
 
-  static get styles() {
-    return css`
-      .nodeSelect {
-        width: 100%;
-      }
-
-      sl-select {
-        --sl-input-border-width: 0px;
-        --sl-input-padding: 0px; // Adjust the padding as needed
-      }
-
-      sl-select::part(listbox) {
-        width: 250px;
-        height: 250px;
-      }
-
-      sl-select::part(display-input) {
-        border-width: 0px;
-        border: none;
-        border-color: transparent;
-        font-weight: 500;
-        color: #0084c7;
-        font-size: 12px; // Adjust the font size as needed
-        width: 70px;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        text-align: left;
-      }
-
-      .icon-header {
-        display: flex;
-        align-items: center;
-        gap: 7px;
-      }
-
-      .test {
-        --sl-input-border-width: 1px;
-      }
-    `;
-  }
-
+  */
   firstUpdated() {
-    // Create a MutationObserver to listen for changes to the 'open' attribute
-    const observer = new MutationObserver((mutations) => {
+    const openObserver = new MutationObserver((mutations) => {
       mutations.forEach((mutation) => {
         if (
           mutation.type === "attributes" &&
           mutation.attributeName === "open"
         ) {
-          const isOpen = this.selectElement.hasAttribute("open");
-          this.onOpenChange(isOpen);
+          this.onOpenChange(this.selectElement.hasAttribute("open"));
         }
       });
     });
-
-    // Start observing the select element for attribute changes
-    observer.observe(this.selectElement, { attributes: true });
+    openObserver.observe(this.selectElement, { attributes: true });
   }
 
   /*
-  
+
   */
   render() {
     const data = this.nodeEditor.editor.drawflow.drawflow.Home.data;
     const nodeId = this.selectedNode.id;
-
-    const filteredNodes = Object.keys(data).filter(
-      (key) =>
-        data[key].id !== nodeId &&
-        data[key].data.title
-          .toLowerCase()
-          .includes(this.searchTerm.toLowerCase())
-    );
-
-    const filterAndMapOptions = (nodeClass) =>
-      filteredNodes
-        .filter((key) => data[key].class === nodeClass)
+    const options = (nodeClass) =>
+      Object.keys(data)
+        .filter(
+          (key) => data[key].id !== nodeId && data[key].class === nodeClass
+        )
         .map(
           (key) => html`<sl-option
             value="${data[key].id}"
+            class="node-option-visible ${data[key].class}"
             @mouseenter=${() => this.nodeEditor._highlightNode(data[key].id)}
             @mouseleave=${() => this.nodeEditor._unhighlightNode(data[key].id)}
-          >
-            ${data[key].data.title}
-          </sl-option>`
+            >${data[key].data.title}</sl-option
+          >`
         );
 
     const hasNodesOfClass = (nodeClass) =>
-      filteredNodes.some((key) => data[key].class === nodeClass);
+      Object.keys(data).some(
+        (key) => data[key].id !== nodeId && data[key].class === nodeClass
+      );
 
-    return html` <sl-select
-      placement="bottom"
-      hoist
-      class="nodeSelect"
-      size="small"
-      placeholder="Not connected"
-      clearable
-      @sl-input=${this._handleUserInputTargetPage}
-      .value=${this.selectedNode?.outputs?.[
-        this.outputClass.toString()
-      ]?.connections?.[0]?.node.toString() ?? "-1"}
-      @mouseenter=${() =>
-        this.highlightConnectionAndNode(
-          this.selectedNode.id,
-          this.selectedNode?.outputs?.[
-            this.outputClass.toString()
-          ]?.connections?.[0]?.node.toString(),
-          this.outputClass,
-          "input_1",
-          this.selectedNode?.outputs?.[
-            this.outputClass.toString()
-          ]?.connections?.[0]?.node.toString()
-        )}
-      @mouseleave=${() =>
-        this.unhighlightConnectionAndNode(
-          this.selectedNode.id,
-          this.selectedNode?.outputs?.[
-            this.outputClass.toString()
-          ]?.connections?.[0]?.node.toString(),
-          this.outputClass,
-          "input_1",
-          this.selectedNode?.outputs?.[
-            this.outputClass.toString()
-          ]?.connections?.[0]?.node.toString()
-        )}
-    >
-      <div style="padding: 10px">
-        <sl-input
-          placeholder="Search..."
-          @sl-input=${this.handleSearch}
-          @click=${() => this.searchElement.focus()}
-          @keydown=${this.handleKeydown}
-          clearable
-          class="test"
-        >
-          <sl-icon src=${search} slot="prefix"></sl-icon>
-        </sl-input>
-      </div>
-      <sl-divider></sl-divider>
-      ${filteredNodes.length === 0
-        ? html`<small>No nodes found</small>`
-        : html`
-            ${hasNodesOfClass("page") || hasNodesOfClass("origin")
-              ? html`
-                  <small class="icon-header"
-                    ><sl-icon src="${file}"></sl-icon> Pages</small
-                  >
-                  ${filterAndMapOptions("page")}
-                  ${filterAndMapOptions("origin")}
-                  ${hasNodesOfClass("popup") || hasNodesOfClass("branch")
-                    ? html`<sl-divider></sl-divider>`
-                    : ""}
-                `
-              : ""}
-            ${hasNodesOfClass("popup")
-              ? html`
-                  <small class="icon-header"
-                    ><sl-icon src="${squares}"></sl-icon> Popup</small
-                  >
-                  ${filterAndMapOptions("popup")}
-                  ${hasNodesOfClass("branch")
-                    ? html`<sl-divider></sl-divider>`
-                    : ""}
-                `
-              : ""}
-            ${hasNodesOfClass("branch")
-              ? html`
-                  <small class="icon-header"
-                    ><sl-icon src="${arrowsSplit2}"></sl-icon> Smart
-                    Branch</small
-                  >
-                  ${filterAndMapOptions("branch")}
-                `
-              : ""}
-          `}
-    </sl-select>`;
+    return html`
+      <sl-select
+        placement="bottom"
+        hoist
+        class="nodeSelect"
+        size="small"
+        placeholder="Not connected"
+        clearable
+        .value=${this.selectedNode?.outputs?.[this.outputClass]
+          ?.connections?.[0]?.node ?? "-1"}
+        @sl-input=${this._handleUserInputTargetPage}
+        @mouseenter=${() => this._highlightConnectionAndNode()}
+        @mouseleave=${() => this._unhighlightConnectionAndNode()}
+      >
+        <div style="padding: 10px">
+          <sl-input
+            placeholder="Search..."
+            @sl-input=${this.handleSearch}
+            @click=${() => this.searchElement.focus()}
+            @keydown=${this.handleKeydown}
+            clearable
+            class="test"
+          >
+            <sl-icon src=${search} slot="prefix"></sl-icon>
+          </sl-input>
+        </div>
+        <sl-divider></sl-divider>
+        ${Object.keys(data).length === 1
+          ? html`<small>No nodes found</small>`
+          : html`
+              ${hasNodesOfClass("page") || hasNodesOfClass("origin")
+                ? html`
+                    <small class="icon-header" id="divider-page"
+                      ><sl-icon src="${file}"></sl-icon> Pages</small
+                    >
+                    ${options("page")} ${options("origin")}
+                    ${hasNodesOfClass("popup") || hasNodesOfClass("branch")
+                      ? html`<sl-divider id="divider-page"></sl-divider>`
+                      : ""}
+                  `
+                : ""}
+              ${hasNodesOfClass("popup")
+                ? html`
+                    <small class="icon-header" id="divider-popup"
+                      ><sl-icon src="${squares}"></sl-icon> Popup</small
+                    >
+                    ${options("popup")}
+                    ${hasNodesOfClass("branch")
+                      ? html`<sl-divider id="divider-popup"></sl-divider>`
+                      : ""}
+                  `
+                : ""}
+              ${hasNodesOfClass("branch")
+                ? html`
+                    <small class="icon-header" id="divider-branch"
+                      ><sl-icon src="${arrowsSplit2}"></sl-icon> Smart
+                      Branch</small
+                    >
+                    ${options("branch")}
+                  `
+                : ""}
+              <small id="no-nodes-found" style="display: none"
+                >No nodes found</small
+              >
+            `}
+      </sl-select>
+    `;
   }
 
   /*
-    Prevent space key from closing the dropdown when typing in the input
+
+  */
+  private handleSearch(event: Event) {
+    const searchTerm = (event.target as HTMLInputElement).value.toLowerCase();
+    this.searchTerm = searchTerm;
+
+    this.shadowRoot.querySelectorAll("sl-option").forEach((option) => {
+      const title = option.textContent.toLowerCase();
+      option.classList.toggle(
+        "node-option-visible",
+        title.includes(searchTerm)
+      );
+      option.classList.toggle(
+        "node-option-hidden",
+        !title.includes(searchTerm)
+      );
+    });
+
+    this._toggleDividers(
+      "page",
+      this.hasVisibleNodesOfClass("page") ||
+        this.hasVisibleNodesOfClass("origin")
+    );
+    this._toggleDividers("popup", this.hasVisibleNodesOfClass("popup"));
+    this._toggleDividers("branch", this.hasVisibleNodesOfClass("branch"));
+    (
+      this.shadowRoot.querySelector("#no-nodes-found") as HTMLElement
+    ).style.display = this._hasNoVisibleNodes() ? "block" : "none";
+  }
+
+  /*
+
+  */
+  private _toggleDividers(dividerId: string, shouldDisplay: boolean) {
+    const displayStyle = shouldDisplay ? "block" : "none";
+    this.shadowRoot
+      .querySelectorAll(`#divider-${dividerId}`)
+      .forEach((element) => {
+        (element as HTMLElement).style.display = displayStyle;
+      });
+  }
+
+  /*
+
+  */
+  private _hasNoVisibleNodes() {
+    return !(
+      this.hasVisibleNodesOfClass("page") ||
+      this.hasVisibleNodesOfClass("origin") ||
+      this.hasVisibleNodesOfClass("popup") ||
+      this.hasVisibleNodesOfClass("branch")
+    );
+  }
+
+  /*
+
+  */
+  private hasVisibleNodesOfClass(nodeClass: string) {
+    return (
+      this.shadowRoot.querySelectorAll(`.node-option-visible.${nodeClass}`)
+        .length > 0
+    );
+  }
+
+  /*
+
   */
   private handleKeydown(event: KeyboardEvent) {
     if (event.target instanceof SlInput) {
       this.searchElement.focus();
-      event.stopPropagation(); // Prevent event propagation
+      event.stopPropagation();
     }
   }
 
   /*
-  
-  */
-  private handleSearch = (event) => {
-    this.searchTerm = event.target.value;
-  };
 
-  /*
-  
   */
-  private _handleUserInputTargetPage(event) {
+  private _handleUserInputTargetPage(event: Event) {
     if (event.target instanceof SlSelect) {
-      if (
-        this.selectedNode?.outputs?.[this.outputClass.toString()] != undefined
-      ) {
-        //initial set
-        if (
-          this.selectedNode?.outputs?.[this.outputClass.toString()]
-            ?.connections?.[0]?.node == undefined &&
-          event.target.value != ""
-        ) {
-          this.nodeEditor.editor.addConnection(
-            this.selectedNode.id,
-            event.target.value,
-            this.outputClass,
-            "input_1"
-          );
-        }
-        //change existing selection
-        else if (
-          this.selectedNode?.outputs?.[this.outputClass.toString()]
-            ?.connections?.[0]?.node != undefined &&
-          event.target.value != ""
-        ) {
-          //get the right output using the answers index which corresponds to the output index
-          this.nodeEditor.editor.removeSingleConnection(
-            this.selectedNode.id,
-            this.selectedNode?.outputs?.[this.outputClass.toString()]
-              ?.connections?.[0]?.node,
-            this.outputClass,
-            "input_1"
-          );
+      const selectedValue = (event.target as SlSelect).value;
+      const connections =
+        this.selectedNode?.outputs?.[this.outputClass]?.connections;
 
-          this.nodeEditor.editor.addConnection(
-            this.selectedNode.id,
-            event.target.value,
-            this.outputClass,
-            "input_1"
-          );
-        }
-        //clear sl-select
-        else if (event.target.value == "") {
-          console.log("on clear");
-          this.unhighlightConnectionAndNode(
-            this.selectedNode.id,
-            this.selectedNode?.outputs?.[
-              this.outputClass.toString()
-            ]?.connections?.[0]?.node.toString(),
-            this.outputClass,
-            "input_1",
-            this.selectedNode?.outputs?.[
-              this.outputClass.toString()
-            ]?.connections?.[0]?.node.toString()
-          );
-          this.nodeEditor.editor.removeSingleConnection(
-            this.selectedNode.id,
-            this.selectedNode?.outputs?.[this.outputClass.toString()]
-              ?.connections?.[0]?.node,
-            this.outputClass,
-            "input_1"
-          );
-        }
-      }
-    }
-  }
-
-  private highlightConnectionAndNode(
-    outputNodeId,
-    inputNodeId,
-    outputClass,
-    inputClass,
-    highlightNodeId
-  ) {
-    if (!this.selectElement.open) {
-      if (inputNodeId) {
-        this.nodeEditor.highlightConnectionAndNode(
-          outputNodeId,
-          inputNodeId,
-          outputClass,
-          inputClass,
-          highlightNodeId
+      if (connections?.[0]?.node === undefined && selectedValue) {
+        this.nodeEditor.editor.addConnection(
+          this.selectedNode.id,
+          selectedValue,
+          this.outputClass,
+          "input_1"
+        );
+      } else if (connections?.[0]?.node !== undefined && selectedValue) {
+        this.nodeEditor.editor.removeSingleConnection(
+          this.selectedNode.id,
+          connections[0].node,
+          this.outputClass,
+          "input_1"
+        );
+        this.nodeEditor.editor.addConnection(
+          this.selectedNode.id,
+          selectedValue,
+          this.outputClass,
+          "input_1"
+        );
+      } else if (!selectedValue) {
+        this._unhighlightConnectionAndNode();
+        this.nodeEditor.editor.removeSingleConnection(
+          this.selectedNode.id,
+          connections?.[0]?.node,
+          this.outputClass,
+          "input_1"
         );
       }
     }
   }
 
-  private unhighlightConnectionAndNode(
-    outputNodeId,
-    inputNodeId,
-    outputClass,
-    inputClass,
-    highlightNodeId
-  ) {
-    if (inputNodeId) {
-      this.nodeEditor.unhighlightConnectionAndNode(
-        outputNodeId,
+  /*
+
+  */
+  private _highlightConnectionAndNode() {
+    const inputNodeId =
+      this.selectedNode?.outputs?.[
+        this.outputClass
+      ]?.connections?.[0]?.node.toString();
+    if (!this.selectElement.open && inputNodeId) {
+      this.nodeEditor.highlightConnectionAndNode(
+        this.selectedNode.id,
         inputNodeId,
-        outputClass,
+        this.outputClass,
         "input_1",
-        highlightNodeId
+        inputNodeId
       );
     }
   }
 
-  private onOpenChange(isOpen: boolean) {
-    if (isOpen) {
-      this.unhighlightConnectionAndNode(
+  /*
+
+  */
+  private _unhighlightConnectionAndNode() {
+    const inputNodeId =
+      this.selectedNode?.outputs?.[
+        this.outputClass
+      ]?.connections?.[0]?.node.toString();
+    if (inputNodeId) {
+      this.nodeEditor.unhighlightConnectionAndNode(
         this.selectedNode.id,
-        this.selectedNode?.outputs?.[
-          this.outputClass.toString()
-        ]?.connections?.[0]?.node.toString(),
+        inputNodeId,
         this.outputClass,
         "input_1",
-        this.selectedNode?.outputs?.[
-          this.outputClass.toString()
-        ]?.connections?.[0]?.node.toString()
+        inputNodeId
       );
+    }
+  }
+
+  /*
+
+  */
+  private onOpenChange(isOpen: boolean) {
+    const nodeId =
+      this.selectedNode?.outputs?.[
+        this.outputClass
+      ]?.connections?.[0]?.node.toString();
+    if (isOpen) {
+      this._unhighlightConnectionAndNode();
       this.nodeEditor._highlightOutput(this.selectedNode.id, this.outputClass);
     } else {
       this.nodeEditor._unhighlightOutput(
